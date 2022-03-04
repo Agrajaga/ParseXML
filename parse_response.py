3f82a1b9291c48c2a620556d3b8b396a837ceda2
Michael <soplomiha@gmail.com>
2022-03-04 13:39:03 +0300

    Add docstrings

--- parse_response.py
+++ parse_response.py
@@ -5,6 +5,7 @@ from xml.etree import cElementTree as ET
 
 
 def descript_flight(flight: ET.Element) -> dict:
+    """ Populate the dictionary with flight attributes"""
     flight_desc = {}
     flight_desc["Carrier_id"] = flight.find("Carrier").get("id")
     for flight_attrib in flight:
@@ -15,11 +16,18 @@ def descript_flight(flight: ET.Element) -> dict:
 
 
 def get_fare_basis(variant: ET.Element) -> str:
+    """ Find and format FareBasis value"""
     return variant.find(
         "OnwardPricedItinerary/Flights/Flight/FareBasis").text.strip()
 
 
 def get_fare(variant: ET.Element, type: str) -> float:
+    """ Finds the total cost of the flight for the specified type of passenger
+
+        *type* one of these values: SingleAdult, SingleChild, SingleInfant
+
+        Return 0.0 if selected type is not found
+    """
     xpath = f".//ServiceCharges[@type='{type}'][@ChargeType='TotalAmount']"
     service_charge = variant.find(xpath)
     if service_charge is None:
@@ -28,11 +36,14 @@ def get_fare(variant: ET.Element, type: str) -> float:
 
 
 def get_passengers_counts(fare_basis: str) -> tuple[int, int, int]:
+    """ Parses the FareBasis code and returns the number of passengers
+     of various types: adults, children, infants"""
     counts_str = fare_basis.split("__A")[1]
     return tuple(map(int, counts_str.split("_")))
 
 
 def calc_total_cost(variant: ET.Element) -> float:
+    """ Calculate the total cost of the flight"""
     fare_basis = get_fare_basis(variant)
     adult_count, child_count, infant_count = get_passengers_counts(fare_basis)
 
@@ -43,6 +54,12 @@ def calc_total_cost(variant: ET.Element) -> float:
 
 
 def calc_flight_time(variant: ET.Element, branch_name: str) -> int:
+    """ Calculate in seconds the flight time in the selected direction
+
+        *branch_name* for onward or return direction
+
+        Return 0 if selected direction is empty
+    """
     departure_ts = variant.find(
         f"./{branch_name}//Flight[1]/DepartureTimeStamp")
     arrival_ts = variant.find(
@@ -57,12 +74,15 @@ def calc_flight_time(variant: ET.Element, branch_name: str) -> int:
 
 
 def calc_total_time(variant: ET.Element) -> int:
+    """ Calculate in seconds the total flight time"""
     onward_time = calc_flight_time(variant, "OnwardPricedItinerary")
     return_time = calc_flight_time(variant, "ReturnPricedItinerary")
     return onward_time + return_time
 
 
 def parse_response(filename: str) -> dict:
+    """ Parses XML and populates a list of flight variants\
+         and search options"""
     tree = ET.parse(filename)
     root = tree.getroot()
 
@@ -86,15 +106,15 @@ def parse_response(filename: str) -> dict:
         variant_desc["total_cost"] = calc_total_cost(variant)
         variant_desc["total_seconds"] = calc_total_time(variant)
 
-        options = {}
-        options["filename"] = filename
-        options["roundtrip"] = bool(flight_desc["return"])
-        passengers_counts = get_passengers_counts(fare_basis)
-        options["adults"] = passengers_counts[0]
-        options["childs"] = passengers_counts[1]
-        options["infants"] = passengers_counts[2]
-
         variants.append(variant_desc)
+
+    options = {}
+    options["filename"] = filename
+    options["roundtrip"] = bool(flight_desc["return"])
+    adult_count, child_count, infant_count = get_passengers_counts(fare_basis)
+    options["adults"] = adult_count
+    options["childs"] = child_count
+    options["infants"] = infant_count
 
     return {
         "options": options,
@@ -103,6 +123,7 @@ def parse_response(filename: str) -> dict:
 
 
 def get_distinctions(response1: dict, response2: dict) -> tuple:
+    """ Compares the search options and returns distinctions"""
     options1 = {}
     options2 = {}
     for option in response1["options"]:
@@ -117,6 +138,8 @@ def get_optimal(
         variants: list,
         cost_weight: float = 0.5,
         time_weight: float = 0.5) -> dict:
+    """ Find the optimal flight by time and cost"""
+
     cost_sum, time_sum = 0, 0
     for variant in variants:
         cost_sum += variant["total_cost"]
@@ -130,6 +153,7 @@ def get_optimal(
 
 
 def get_all_variants(filename: str) -> list:
+    """ Create a list of all flight variants"""
     flights = []
     response = parse_response(filename)
     for variant in response["variants"]:
@@ -138,6 +162,7 @@ def get_all_variants(filename: str) -> list:
 
 
 def get_best_variants(filename: str) -> dict:
+    """ Calculate cheapest/expensive, fastest/slowest and optimal flight"""
     flights = {}
     variants = parse_response(filename)["variants"]
     variants.sort(key=lambda v: v["total_cost"])
@@ -152,10 +177,11 @@ def get_best_variants(filename: str) -> dict:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Parse XML response from via.com and return JSON")
+        description="Parse the XML response about flights from via.com \
+         and return JSON")
     parser.add_argument("file1", help="XML response from via.com")
     parser.add_argument(
-        "--hr",
+        "--human",
         action="store_true",
         help="human-readable output")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -175,9 +201,7 @@ if __name__ == "__main__":
         help="returns differences in query parameters")
     args = parser.parse_args()
 
-    indent = None
-    if args.hr:
-        indent = " "
+    indent = " " if args.human else None
     if args.all:
         print(json.dumps(get_all_variants(args.file1), indent=indent))
     if args.best:
